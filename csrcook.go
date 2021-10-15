029af74a0d1a59bb578a109d9ca98148184a2b58
Bartosz Pietraszko <bartoszpietraszko@gmail.com>
2021-10-15 22:32:04 +0200

    Allow setting distinguished name fields in certficate request
    Separate function that prepares request template and sets subject fields
    passed in a string map. Also, key size has been parametrized.

--- csrcook.go
+++ csrcook.go
@@ -5,24 +5,25 @@ import "strings"
 import "crypto/rsa"
 import "crypto/rand"
 import "crypto/x509"
+import "crypto/x509/pkix"
 import "encoding/pem"
 
 func main() {
-	csr, key := generateKeyAndCsr()
+	csr, key := generateKeyAndCsr(2048, map[string]string{})
+
 	fmt.Println(csr)
 	fmt.Println(key)
 }
 
-func generateKeyAndCsr() (csr_str string, key_str string) {
-	fmt.Println("Generating RSA keypair...")
+func generateKeyAndCsr(bitsize int, subjectFields map[string]string) (csr_str string, key_str string) {
+	key, error := rsa.GenerateKey(rand.Reader, bitsize)
 
-	key, error := rsa.GenerateKey(rand.Reader, 2048)
 	if error != nil {
 		fmt.Println("Error while generating RSA keypair")
 		fmt.Println(error)
 	}
 
-	tpl := new(x509.CertificateRequest)
+	tpl := prepareRequestTemplate(subjectFields)
 
 	csr, error := x509.CreateCertificateRequest(rand.Reader, tpl, key)
 	if error != nil {
@@ -50,5 +51,34 @@ func generateKeyAndCsr() (csr_str string, key_str string) {
 
 	csr_str = encoded_csr.String()
 	key_str = encoded_key.String()
-	return
+
+	return csr_str, key_str
+}
+
+func prepareRequestTemplate(subjectFields map[string]string) *x509.CertificateRequest {
+	tpl := new(x509.CertificateRequest)
+	subject := new(pkix.Name)
+
+	if subjectFields["C"] != "" {
+		subject.Country = []string{subjectFields["C"]}
+	}
+	if subjectFields["O"] != "" {
+		subject.Organization = []string{subjectFields["O"]}
+	}
+	if subjectFields["OU"] != "" {
+		subject.OrganizationalUnit = []string{subjectFields["OU"]}
+	}
+	if subjectFields["ST"] != "" {
+		subject.Province = []string{subjectFields["ST"]}
+	}
+	if subjectFields["L"] != "" {
+		subject.Locality = []string{subjectFields["L"]}
+	}
+	if subjectFields["CN"] != "" {
+		subject.CommonName = subjectFields["CN"]
+	}
+
+	tpl.Subject = *subject
+
+	return tpl
 }
